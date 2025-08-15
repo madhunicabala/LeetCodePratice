dac03bedd2ba2a304ad3f7d5235e492e0495e2fb
Madhunica B <madhunicabala.92@gmail.com>
2025-08-14 20:37:50 -0700

    Update LC_268.cpp

--- LC_268.cpp
+++ LC_268.cpp
@@ -12,22 +12,48 @@ Explanation:
 n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
 */
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
+#include<iostream>
+#include <vector>
+using namespace std;
+ 
+class Solution
+{
+    public:
+            int missingElement(vector<int>&array);
+};
+
+int Solution::missingElement(vector<int>&array) {
     int xor_operation1 = 0;
     int xor_operation2 = 0;
+    cout << "Capacity is " << array.capacity() << std::endl;
+    cout << "Size is " << array.size() << std::endl;
 
-
-    for (int j =0; j < nums.size(); j++){
-        xor_operation1^=nums.at(j);
+    for (int j =0; j < array.size(); j++){
+        xor_operation1^=array.at(j);
     }
-    for (int k = 0; k <= nums.size() ; k++){
+    for (int k = 1; k <= 5 ; k++){
         xor_operation2^=k;
     }
     int missing_ele = 0;
     missing_ele = xor_operation1 ^ xor_operation2;
     return missing_ele;
-        
+}
+
+int main() {
+    Solution s;
+    int num, element = 0;
+    int missing_element = 0;
+    cout << "Enter Number of elements " << std::endl;
+    cin >> num;
+    vector<int> arr;
+    for (int i =0; i < num ; i++){
+        cout << "Enter element " << i << std::endl;
+        cin >> element;
+        arr.push_back(element);
     }
-};
+    missing_element = s.missingElement(arr);
+    cout << "Missing Element is " << missing_element << std::endl;
+
+    return 0;
+}
+
